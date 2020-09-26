ef0b0f1b05c4e250de610e60277ba907b48c8429
csi <robin256@naver.com>
2020-09-27 00:04:46 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const config = JSON.parse(data);
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    port: config.port,
+    database: config.database    
+});
+conn.connect();
+
 //  https://jsonlint.com/ json 확인
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '이순신',
-            'birthday': '940111',
-            'gender': '남자',
-            'job': '회사원'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '유관순',
-            'birthday': '920331',
-            'gender': '여자',
-            'job': '학생'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '바이브',
-            'birthday': '890123',
-            'gender': '남자',
-            'job': '가수'
-        },
-    ]);
+    const sql = "SELECT * FROM CUSTOMER";
+    conn.query(sql, (err, rows, fields) => {
+        res.send(rows);    
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
