627c07051dc5bb50c213ece18d724fdcd26ff9e5
csi <robin256@naver.com>
2020-09-26 23:16:11 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+//  https://jsonlint.com/ json 확인
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '이순신',
+            'birthday': '940111',
+            'gender': '남자',
+            'job': '회사원'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '유관순',
+            'birthday': '920331',
+            'gender': '여자',
+            'job': '학생'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '바이브',
+            'birthday': '890123',
+            'gender': '남자',
+            'job': '가수'
+        },
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
